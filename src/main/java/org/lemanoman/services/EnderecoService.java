8882fe9e5c36ddf6607590b31c726fae083468f8
kevim <k.v.n.such@gmail.com>
2015-11-04 15:39:45 -0200

    asdasd

--- src/main/java/org/lemanoman/services/EnderecoService.java
+++ src/main/java/org/lemanoman/services/EnderecoService.java
@@ -1,7 +1,12 @@
 package org.lemanoman.services;
 
 import org.lemanoman.model.EnderecoModel;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 public interface EnderecoService{
     public EnderecoModel searchByCEP(String cepSearch);
 }
