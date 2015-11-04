83620efc422f500c97be09eda059303771b35f14
kevim <k.v.n.such@gmail.com>
2015-11-04 15:16:43 -0200

    Teste

---
+++ src/main/java/org/lemanoman/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package org.lemanoman.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.lemanoman.model.GreetingModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public GreetingModel greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	return new GreetingModel(counter.incrementAndGet(),
+		String.format(template, name));
+    }
+}
+
